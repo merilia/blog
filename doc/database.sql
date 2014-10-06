496e696a213f9348088a7d03d50603bfd536d0bf
Merili <meriliasu@gmail.com>
2014-10-06 16:35:19 +0300

    Remove unwanted tags from posts + dbdump

--- doc/database.sql
+++ doc/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Loomise aeg: Okt 06, 2014 kell 03:33 PM
+-- Loomise aeg: Sept 17, 2014 kell 03:15 PM
 -- Serveri versioon: 5.5.39
 -- PHP versioon: 5.4.31
 
@@ -18,6 +18,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tabeli struktuur tabelile `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE IF NOT EXISTS `comment` (
+`comment_id` int(10) unsigned NOT NULL,
+  `comment_text` text NOT NULL,
+  `comment_author` varchar(50) NOT NULL,
+  `comment_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `post_id` int(10) unsigned NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+
+--
+-- Andmete tõmmistamine tabelile `comment`
+--
+
+INSERT INTO `comment` (`comment_id`, `comment_text`, `comment_author`, `comment_created`, `post_id`) VALUES
+(1, 'Tere-tere!', 'Jumal', '2014-09-10 11:39:47', 1),
+(5, 'asdasd', 'Klaabu', '2014-09-10 12:43:02', 1),
+(6, 'asdasd', 'Klaabu', '2014-09-10 12:43:09', 1),
+(7, 'asdasd', 'Klaabu', '2014-09-10 12:44:31', 1),
+(8, 'WEWE', 'Klaabu', '2014-09-10 12:45:57', 1),
+(9, 'asdasd', 'Klaabu', '2014-09-10 13:29:04', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Tabeli struktuur tabelile `post`
 --
 
@@ -28,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `post` (
   `post_text` text NOT NULL,
   `post_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `user_id` int(10) unsigned NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Andmete tõmmistamine tabelile `post`
@@ -38,13 +65,79 @@ INSERT INTO `post` (`post_id`, `post_subject`, `post_text`, `post_created`, `use
 (1, 'Tere, maailm!', 'Hallo, Kosmos!', '2014-09-04 11:42:53', 1),
 (2, 'Tere, maailm!', 'Hallo, Kosmos!', '2014-09-04 11:42:59', 1),
 (3, '', '', '2014-09-04 11:43:21', 1),
-(4, 'hallo', 'jou', '2014-09-17 13:13:58', 1),
-(5, 'Test', 'Test2', '2014-09-17 13:47:42', 1),
-(6, 'afdaf', 'asfasf', '2014-10-02 19:44:18', 1);
+(4, 'hallo', 'jou', '2014-09-17 13:13:58', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `post_tags`
+--
+
+DROP TABLE IF EXISTS `post_tags`;
+CREATE TABLE IF NOT EXISTS `post_tags` (
+  `post_id` int(11) unsigned NOT NULL,
+  `tag_id` int(11) unsigned NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Andmete tõmmistamine tabelile `post_tags`
+--
+
+INSERT INTO `post_tags` (`post_id`, `tag_id`) VALUES
+(1, 1),
+(2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `tag`
+--
+
+DROP TABLE IF EXISTS `tag`;
+CREATE TABLE IF NOT EXISTS `tag` (
+`tag_id` int(10) unsigned NOT NULL,
+  `tag_name` varchar(25) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Andmete tõmmistamine tabelile `tag`
+--
+
+INSERT INTO `tag` (`tag_id`, `tag_name`) VALUES
+(1, 'üks'),
+(2, 'kaks');
+
+-- --------------------------------------------------------
+
+--
+-- Tabeli struktuur tabelile `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+`user_id` int(10) unsigned NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `deleted` tinyint(3) unsigned NOT NULL DEFAULT '0'
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Andmete tõmmistamine tabelile `user`
+--
+
+INSERT INTO `user` (`user_id`, `username`, `password`, `deleted`) VALUES
+(1, 'demo', 'demo', 0),
+(2, 'Merili', '051912748', 0);
 
 --
 -- Indeksid tõmmistatud tabelitele
 --
+
+--
+-- Indeksid tabelile `comment`
+--
+ALTER TABLE `comment`
+ ADD PRIMARY KEY (`comment_id`), ADD KEY `post_id` (`post_id`);
 
 --
 -- Indeksid tabelile `post`
@@ -53,21 +146,73 @@ ALTER TABLE `post`
  ADD PRIMARY KEY (`post_id`), ADD KEY `user_id` (`user_id`);
 
 --
+-- Indeksid tabelile `post_tags`
+--
+ALTER TABLE `post_tags`
+ ADD PRIMARY KEY (`post_id`,`tag_id`), ADD KEY `tag_id` (`tag_id`);
+
+--
+-- Indeksid tabelile `tag`
+--
+ALTER TABLE `tag`
+ ADD PRIMARY KEY (`tag_id`);
+
+--
+-- Indeksid tabelile `user`
+--
+ALTER TABLE `user`
+ ADD PRIMARY KEY (`user_id`);
+
+--
 -- AUTO_INCREMENT tõmmistatud tabelitele
 --
 
 --
+-- AUTO_INCREMENT tabelile `comment`
+--
+ALTER TABLE `comment`
+MODIFY `comment_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+--
 -- AUTO_INCREMENT tabelile `post`
 --
 ALTER TABLE `post`
-MODIFY `post_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+MODIFY `post_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT tabelile `tag`
+--
+ALTER TABLE `tag`
+MODIFY `tag_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT tabelile `user`
+--
+ALTER TABLE `user`
+MODIFY `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- Tõmmistatud tabelite piirangud
 --
+
+--
+-- Piirangud tabelile `comment`
+--
+ALTER TABLE `comment`
+ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`);
 
 --
 -- Piirangud tabelile `post`
 --
 ALTER TABLE `post`
 ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Piirangud tabelile `post_tags`
+--
+ALTER TABLE `post_tags`
+ADD CONSTRAINT `post_tags_ibfk_1` FOREIGN KEY (`post_id`) REFERENCES `post` (`post_id`),
+ADD CONSTRAINT `post_tags_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`tag_id`);
+
+--
+-- Piirangud tabelile `tag`
+--
+ALTER TABLE `tag`
+ADD CONSTRAINT `tag_ibfk_1` FOREIGN KEY (`tag_id`) REFERENCES `post` (`post_id`);
 SET FOREIGN_KEY_CHECKS=1;
