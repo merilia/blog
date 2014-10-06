52e83d65fd6a3d91056f0aa8f7fe3d7ecb806e6f
Merili <meriliasu@gmail.com>
2014-10-06 16:34:09 +0300

    Remove unwanted tags from posts + dbdump

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -3,13 +3,10 @@
     <h1><a href="<?= BASE_URL ?>posts/view/<?= $post['post_id'] ?>"><?=$post['post_subject']?></a></h1>
     <p><?=$post['post_text']?></p>
     <div>
-        <span class="badge badge-success">Posted <?= $post['post_created']?></span><div class="pull-right">
+        <span class="badge badge-success">Posted <?= $post['post_created']?></span>
+        <div class="pull-right">
             <?foreach($tags[$post['post_id']] as $tag):?>
              <a href="#"><span class="label" style="background-color: #5bc0de"><?=$tag?></span></a><? endforeach?>
-            <span class="label">alice</span>
-            <span class="label">story</span>
-            <span class="label">blog</span>
-            <span class="label">personal</span>
         </div>
     </div>
 </div>
